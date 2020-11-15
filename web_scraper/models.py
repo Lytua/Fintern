6f09daebd97ccddb7bdcdc7afae660eae712a443
林昱廷 <yuting@AIRTIM.fdorm3.nccu.edu.tw>
2020-11-15 19:39:50 +0800

    start django project

---
+++ web_scraper/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from djangotoolbox.fields import ListField, EmbeddedModelField
+# Create your models here.
+
+
+class Job(models.Model):
+    
+class CompanyInfo(models.Model):
+    
+class Salary(models.Model):
+    salary = Charfield("")
+    
